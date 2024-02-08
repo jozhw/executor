a6e4e57320ffd6cf4d6ad8fb16c756c71b1e5de0
John W <112655410+jozhw@users.noreply.github.com>
2024-02-08 17:36:26 -0600

    update: readjust deletionerror

--- src/errors/deletion_error.rs
+++ src/errors/deletion_error.rs
@@ -3,19 +3,17 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum DeletionError {
-    IoError(std::io::Error),
-    Utf8Error(std::str::Utf8Error),
-    CommandError(String, std::process::ExitStatus),
+    NotFound,
+    PermissionDenied,
+    Other(String),
 }
 
 impl fmt::Display for DeletionError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            DeletionError::IoError(err) => write!(f, "IO Error: {}", err),
-            DeletionError::Utf8Error(err) => write!(f, "UTF-8 Error: {}", err),
-            DeletionError::CommandError(message, status) => {
-                write!(f, "Command Error: {}. Exit status: {:?}", message, status)
-            }
+            DeletionError::NotFound => write!(f, "File not found"),
+            DeletionError::PermissionDenied => write!(f, "Permission denied"),
+            DeletionError::Other(message) => write!(f, "Error: {}", message),
         }
     }
 }
@@ -23,9 +21,8 @@ impl fmt::Display for DeletionError {
 impl Error for DeletionError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            DeletionError::IoError(err) => Some(err),
-            DeletionError::Utf8Error(err) => Some(err),
-            DeletionError::CommandError(_, _) => None,
+            DeletionError::NotFound | DeletionError::PermissionDenied => None,
+            DeletionError::Other(_) => Some(self),
         }
     }
 }
