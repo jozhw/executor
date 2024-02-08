b9fe93a05ab590b73408fe5bdfd7335b9dc32540
John W <112655410+jozhw@users.noreply.github.com>
2024-02-07 21:45:39 -0600

    fix: utferror -> utf8error

--- src/errors/execution_error.rs
+++ src/errors/execution_error.rs
@@ -4,7 +4,7 @@ use std::fmt;
 #[derive(Debug)]
 pub enum ExecutionError {
     IoError(std::io::Error),
-    UtfError(std::str::Utf8Error),
+    Utf8Error(std::str::Utf8Error),
     CommandError(String, std::process::ExitStatus),
 }
 
@@ -12,7 +12,7 @@ impl fmt::Display for ExecutionError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ExecutionError::IoError(err) => write!(f, "IO Error: {}", err),
-            ExecutionError::UtfError(err) => write!(f, "UTF-8 Error: {}", err),
+            ExecutionError::Utf8Error(err) => write!(f, "UTF-8 Error: {}", err),
             ExecutionError::CommandError(message, status) => {
                 write!(f, "Command Error: {}. Exit status: {:?}", message, status)
             }
@@ -24,7 +24,7 @@ impl Error for ExecutionError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             ExecutionError::IoError(err) => Some(err),
-            ExecutionError::UtfError(err) => Some(err),
+            ExecutionError::Utf8Error(err) => Some(err),
             ExecutionError::CommandError(_, _) => None,
         }
     }
