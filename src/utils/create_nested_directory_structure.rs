5c7e96f53091764f6b1ac0b9173600d948291cec
John W <112655410+jozhw@users.noreply.github.com>
2024-02-10 16:43:04 -0600

    update: comments to functionality and more thorough error handling

--- src/utils/create_nested_directory_structure.rs
+++ src/utils/create_nested_directory_structure.rs
@@ -1,12 +1,23 @@
 use std::{fs, path::PathBuf};
 use tempfile::{tempdir, TempDir};
-// Helper function to create a two nested directory structure
+/// Helper function to create a two nested directory structure for traverse testing
+///
+/// # Arguments
+///
+/// None
+///
+/// # Returns
+///
+/// A TempDir in order to remain inscope for testing
 pub fn create_nested_directory_structure() -> Result<TempDir, std::io::Error> {
-    let temp_dir: TempDir = tempdir()?;
+    // create the temp dir
+    let temp_dir: TempDir = tempdir().expect("Failed to create temporary root directory");
+
+    // create nested directories to attach to root temp dir
     let nested_path: PathBuf = temp_dir.path().join("subdir1/subdir2");
     fs::create_dir_all(&nested_path)?;
 
-    // Create a test file in the nested structure
+    // create a test file in the nested structure
     let file_path: PathBuf = nested_path.join("test_file.txt");
     fs::write(&file_path, "Test content")?;
 
@@ -16,10 +27,9 @@ pub fn create_nested_directory_structure() -> Result<TempDir, std::io::Error> {
 // tests
 #[cfg(test)]
 mod tests {
-    use std::fs;
 
-    // Import the helper function you want to test
     use crate::utils::create_nested_directory_structure::create_nested_directory_structure;
+    use std::fs;
 
     #[test]
     fn test_create_nested_directory_structure() {
@@ -27,15 +37,15 @@ mod tests {
         let temp_dir =
             create_nested_directory_structure().expect("Failed to create nested structure");
 
-        // Check if the nested structure was created successfully
+        // check if the nested structure was created successfully
         let nested_path = temp_dir.path().join("subdir1/subdir2");
         assert!(nested_path.is_dir(), "Nested structure was not created");
 
-        // Check if the test file was created
+        // check if the test file was created
         let file_path = nested_path.join("test_file.txt");
         assert!(file_path.is_file(), "Test file was not created");
 
-        // Check the content of the test file
+        // check the content of the test file
         let content = fs::read_to_string(&file_path).expect("Failed to read test file");
         assert_eq!(
             content.trim(),
